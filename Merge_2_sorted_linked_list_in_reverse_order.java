76f83c706e519673844062457b5b5b1e43e4f5e3
Akash Modak <118994261+Akash-9832@users.noreply.github.com>
2024-01-08 21:30:35 +0530

    Create Merge_2_sorted_linked_list_in_reverse_order.java
    Problem: Given two linked lists of size N and M, which are sorted in non-decreasing order. The task is to merge them in such a way that the resulting list is in non-increasing order.
    
    Example 1:
    Input:
    N = 2, M = 2
    list1 = 1->3
    list2 = 2->4
    Output:
    4->3->2->1
    Explanation:
    After merging the two lists in non-increasing order, we have new lists as 4->3->2->1.

---
+++ Merge_2_sorted_linked_list_in_reverse_order.java
@@ -0,0 +1,36 @@
+class Merge_2_sorted_linked_list_in_reverse_order
+{
+    Node mergeResult(Node node1, Node node2){
+        Node n1 = reverse(node1);
+        Node n2 = reverse(node2);
+        
+        return mergeRecur(n1,n2);
+        
+    }
+    Node mergeRecur(Node A, Node B){
+ 
+        if (A == null) return B;
+        if (B == null) return A;
+ 
+        if (A.data > B.data) {
+            A.next = mergeRecur(A.next, B);
+            return A;
+        }
+        else {
+            B.next = mergeRecur(A, B.next);
+            return B;
+        }
+    }
+    
+    Node reverse(Node node){
+        Node prv = null;
+        Node cur = node;
+        
+        while(cur!=null){
+            Node next = cur.next;
+            cur.next = prv;
+            prv = cur;
+            cur = next;
+        }
+        return prv;
+    }
