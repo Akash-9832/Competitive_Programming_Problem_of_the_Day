8975212694fd63a37d39cfe17f96fe2b83dc9ad6
Akash Modak <118994261+Akash-9832@users.noreply.github.com>
2024-01-19 23:59:09 +0530

    Update Missing_num.java

--- Missing_num.java
+++ Missing_num.java
@@ -1,6 +1,6 @@
 public class Missing_num {
     public static void main(String[] args) {
-        int arr[]={2,1,5,4,3,6,8};
+        int arr[]={3,1,5,4,7,6,8};
         int n=arr.length+1;
         int sum=(n*(n+1))/2;
         for(int i:arr){
