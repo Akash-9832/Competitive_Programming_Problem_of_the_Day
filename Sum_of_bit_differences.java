dae8e623f7c6a7736afa2120dd87b7f32cffa7f3
Akash Modak <118994261+Akash-9832@users.noreply.github.com>
2024-02-29 15:14:24 +0530

    Create Sum_of_bit_differences.java
    Problem: Given an array integers arr[], containing n elements, find the sum of bit differences between all pairs of element in the array. Bit difference of a pair (x, y) is the count of different bits at the same positions in binary representations of x and y.
    For example, bit difference for 2 and 7 is 2. Binary representation of 2 is 010 and 7 is 111 respectively and the first and last bits differ between the two numbers.
    Note: (x, y) and (y, x) are considered two separate pairs.
    
    Example 1:
    Input: 
    n = 2
    arr[] = {1, 2}
    Output: 4
    Explanation: All possible pairs of an array are (1, 1), (1, 2), (2, 1), (2, 2).
    Sum of bit differences = 0 + 2 + 2 + 0 = 4

---
+++ Sum_of_bit_differences.java
@@ -0,0 +1,26 @@
+public class Sum_of_bit_differences
+{
+	public static void main(String[] args) {
+	    int[] arr = {1, 3, 5};
+	    Solution obj = new Solution();
+		System.out.println(obj.sumBitDifferences(arr,arr.length));
+	}
+}
+
+class Solution {
+    long sumBitDifferences(int[] arr, int n) {
+       long sum=0;
+       for(int bit = 1;bit<18;bit++) {
+            long one = 0, zero = 0;
+            for(int i = 0;i<n;i++) {
+                if((arr[i] & 1)==1)
+                    one++;
+                else
+                    zero++;
+                arr[i] = arr[i]>>1;
+            }
+            sum = sum+(one*zero);
+        }
+        return sum*2;
+    }
+}
